9aaf853b9341c2f0e757ba24b979ca1d7145ff2b
Бауыржан <@->
2019-05-02 15:29:33 +0600

    Добавьте файлы проекта.

---
+++ DapperNews.ConsoleApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using DapperNews.Models;
+using DapperNews.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperNews.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tГлавное меню");
+            Console.WriteLine("Выберите:");
+            Console.WriteLine("Добавление (нажмите 1)");
+            Console.WriteLine("Удаление (нажмите 2)");
+
+            NewsRepository _newsRep = new NewsRepository();
+
+            int choise = int.Parse(Console.ReadLine());
+            if (choise == 1)
+            {
+                do
+                {
+                    Console.Write("Новости: ");
+                    var news = Console.ReadLine();
+                    Console.Write("Комментарий: ");
+                    var comment = Console.ReadLine();
+
+                    _newsRep.InsertNews(@"INSERT INTO News (NewsNews, Comment)
+                                        VALUES(@NewsNews, @Comment)",
+                new News()
+                {
+                    NewsNews = news,
+                    Comment = comment
+                });
+
+                    var newsN = _newsRep.GetAllNews("SELECT * FROM News");
+
+                    newsN.ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.NewsNews}\t\t{f.Comment}");
+                    });
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+
+            else if (choise == 2)
+            {
+                do
+                {
+                    _newsRep.GetAllNews("SELECT * FROM News").ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.NewsNews}\t\t{f.Comment}");
+                    });
+                    Console.WriteLine("Введите идентификатор для удаления:");
+                    var tmp = Console.ReadLine();
+                    int IdSt;
+                    int.TryParse(tmp, out IdSt);
+
+                    _newsRep.DeleteNews(@"DELETE News WHERE Id = @Id", IdSt);
+
+                    var newsN = _newsRep.GetAllNews("SELECT * FROM News");
+                    newsN.ForEach(f =>
+                    {
+                        Console.WriteLine($"{f.Id}\t\t{f.NewsNews}\t\t{f.Comment}");
+                    });
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+
+        }
+    }
+}
---
+++ DapperNews.DataAccess/NewsRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperNews.Models;
+
+namespace DapperNews.DataAccess
+{
+    public class NewsRepository
+    {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\бауыржан\мой документы\visual studio 2015\Projects\DapperNews.ConsoleApp\DapperNews.DataAccess\NewsDB.mdf;Integrated Security=True";
+
+        //Method InsertNews
+        public string InsertNews(string query, News news)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                var result = sql.Execute(query, news);
+                if (result < 1) throw new Exception("Ошибка при вставке записи новостей");
+            }
+            return "Вставка произошла успешно";
+        }
+
+        //Method Delete
+        public void DeleteNews(string query, int id)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+
+                sql.Execute(query, new { Id = id });
+            }
+            //return "Удаление произошла успешно";
+        }
+
+
+        //Create method GetAllNews
+        public List<News> GetAllNews(string query)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                return sql.Query<News>(query).ToList();
+            }
+        }
+
+        public int Insert<T>(string query, T data)
+        {
+            using (var sql = new SqlConnection(connectionString))
+            {
+                return sql.Execute(query, data);
+            }
+        }
+
+    }
+}
